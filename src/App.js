a0b9c4baccc964b70916d634316e14857968da63
TruongNX <test@gmail.com>
2024-11-16 19:41:33 +0700

    update login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate , useLocation } from "react-router-dom";
 import "./App.css";
 import SideBar from "./component/SideBar";
 import Header from "./component/Header";
 import Footer from "./component/Footer.jsx";
+import { getRoleFromToken } from "./api/DecodeToken.js"; 
+import { introspect } from "./api/Authentication.js";
 
 import Login from "./common/login/Login.jsx";
 import EventDescription from "./common/event-description/EventDescription.jsx";
@@ -35,159 +37,94 @@ import StudentManage from "./admin/student-manage/StudentManage.jsx";
 import ScheduleManage from "./admin/schedule-manage/ScheduleManage.jsx";
 import SemesterManage from "./admin/semester-manage/SemesterManage.jsx";
 
-const userAccount = {
-  id: 1,
-  code: "ps27456",
-  name: "Tiểu Bạc Liêu",
-  codeUser: "lieutbps27456",
-  email: "lieutbps27456@fpt.edu.vn",
-  ownEmail: "lieu123@gmail.com",
-  gender: "Nam",
-  birthday: "22/02/1998",
-  phone: "0902888888",
-  address: "23 AAABBBCCCC, Q1,P9",
-  Specialization: "Công nghệ thông tin",
-  major: "Phát triển phần mềm",
-  avatar:
-    "https://static.vecteezy.com/system/resources/previews/011/459/666/original/people-avatar-icon-png.png",
-  role: "student",
-};
 
 function App() {
-  const userRole = userAccount.role;
+  const [userRole, setUserRole] = useState(null);
+  const navigate = useNavigate();
+  const location = useLocation();
+  useEffect(() => {
+    if (location.pathname === "/login") return;
+    const checkToken = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        alert("Vui lòng đăng nhập.");
+        navigate("/login");
+        return;
+      }
+      const introspectResponse = await introspect(token);
+        if (!introspectResponse?.data?.valid) {
+          alert("Phiên đăng nhập hết hạn. Vui lòng đăng nhập lại.");
+          localStorage.removeItem("token");
+          navigate("/login");
+          return;
+        }
+        const role = getRoleFromToken();
+        setUserRole(role);
+    };
 
-  // const [padding, setPadding] = useState("");
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     if (window.innerWidth <= 783) {
-  //       setPadding("pt-[69px]");
-  //     } else {
-  //       setPadding("");
-  //     }
-  //   };
-  //   window.addEventListener("resize", handleResize);
-  //   // Kiểm tra kích thước màn hình khi component được mount
-  //   handleResize();
-  //   return () => {
-  //     window.removeEventListener("resize", handleResize);
-  //   };
-  // }, [padding]);
+    checkToken(); 
+  }, [navigate,location.pathname]); 
 
   return (
-    <BrowserRouter>
       <div className="flex">
-        {/* <Route path="/" element={<HomePage />} /> */}
-        <SideBar userRole={userRole} user={userAccount} />
-
+        <SideBar userRole={userRole} />
         <div className="flex-1 h-full">
-          <Header userRole={userRole} user={userAccount} />
-
-          <div className={`mt-1 pt-[69px] px-4 md:pt-0`}>
+          <Header userRole={userRole} />
+          <div className="mt-1 pt-[69px] px-4 md:pt-0">
             <Routes>
+               {/* Redirect mặc định sang login */}
+               <Route path="/" element={<Navigate to="/login" />} />
               {/* Common */}
-              <Route path="/" element={<Login />} />
-              <Route path="/event/:title" element={<EventDescription />} />
-              <Route
-                path="/person-info/:code/:name"
-                element={<PersonalInformation user={userAccount} />}
-              />
+              <Route path="/login" element={<Login />} />           
 
               {/* Student */}
-
-              <Route path="/student/home" element={<HomePage />} />
-              <Route
-                path="/student/study-schedule"
-                element={<StudySchedule user={userAccount} />}
-              />
-              <Route
-                path="/student/exam-schedule"
-                element={<ExamSchedule user={userAccount} />}
-              />
-              <Route
-                path="/student/current-subject"
-                element={<CurrentSubject user={userAccount} />}
-              />
-              <Route
-                path="/student/change-schedule"
-                element={<ChangeSchedule user={userAccount} />}
-              />
-              <Route
-                path="/student/register-subject"
-                element={<RegisterSubject user={userAccount} />}
-              />
-              <Route
-                path="/student/study-history"
-                element={<StudyHistory user={userAccount} />}
-              />
-              <Route
-                path="/student/calendar"
-                element={<Calendar user={userAccount} />}
-              />
-              <Route
-                path="/student/find-subject"
-                element={<FindSubject user={userAccount} />}
-              />
+              {userRole === "ROLE_STUDENT" && (
+                <>
+                  <Route path="/student/home" element={<HomePage />} />
+                  <Route path="/event/:title" element={<EventDescription />} />
+                  <Route path="/person-info/" element={<PersonalInformation />} />
+                  <Route path="/student/study-schedule" element={<StudySchedule />} />
+                  <Route path="/student/exam-schedule" element={<ExamSchedule />} />
+                  <Route path="/student/current-subject" element={<CurrentSubject />} />
+                  <Route path="/student/change-schedule" element={<ChangeSchedule />} />
+                  <Route path="/student/register-subject" element={<RegisterSubject />} />
+                  <Route path="/student/study-history" element={<StudyHistory />} />
+                  <Route path="/student/calendar" element={<Calendar />} />
+                  <Route path="/student/find-subject" element={<FindSubject />} />
+                </>
+              )}
 
               {/* Instructor */}
-              <Route path="/instructor/home" element={<HomePageInstructor />} />
-              <Route
-                path="/instructor/teach-day"
-                element={<TeachDay user={userAccount} />}
-              />
-              <Route
-                path="/instructor/check-attendance/:code/:clazz"
-                element={<CheckAttendance user={userAccount} />}
-              />
-              <Route
-                path="/instructor/student-list/:code/:clazz"
-                element={<StudentList user={userAccount} />}
-              />
-              <Route
-                path="/instructor/find-subject"
-                element={<FindSubjectInstructor user={userAccount} />}
-              />
-              <Route
-                path="/instructor/teach-manage"
-                element={<TeachManage user={userAccount} />}
-              />
-              <Route
-                path="/instructor/offed-replace"
-                element={<OffedReplace user={userAccount} />}
-              />
-              <Route
-                path="/instructor/exam-arrange/:code/:clazz"
-                element={<ExamArrange user={userAccount} />}
-              />
+              {userRole === "ROLE_INSTRUCTOR" && (
+                <>
+                  <Route path="/instructor/home" element={<HomePageInstructor />} />
+                  <Route path="/event/:title" element={<EventDescription />} />
+                  <Route path="/instructor/teach-day" element={<TeachDay />} />
+                  <Route path="/instructor/check-attendance/:code/:clazz" element={<CheckAttendance />} />
+                  <Route path="/instructor/student-list/:code/:clazz" element={<StudentList />} />
+                  <Route path="/instructor/find-subject" element={<FindSubjectInstructor />} />
+                  <Route path="/instructor/teach-manage" element={<TeachManage />} />
+                  <Route path="/instructor/offed-replace" element={<OffedReplace />} />
+                  <Route path="/instructor/exam-arrange/:code/:clazz" element={<ExamArrange />} />
+                </>
+              )}
 
               {/* Admin */}
-              <Route path="/admin/statistics" element={<Statistic />} />
-              <Route
-                path="/admin/class-manage"
-                element={<ClassManage user={userAccount} />}
-              />
-              <Route
-                path="/admin/testday-manage"
-                element={<TestdayManage user={userAccount} />}
-              />
-              <Route
-                path="/admin/student-manage"
-                element={<StudentManage user={userAccount} />}
-              />
-              <Route
-                path="/admin/schedule-manage"
-                element={<ScheduleManage user={userAccount} />}
-              />
-              <Route
-                path="/admin/semester-manage"
-                element={<SemesterManage user={userAccount} />}
-              />
+              {userRole === "ROLE_ADMIN" && (
+                <>
+                  <Route path="/admin/statistics" element={<Statistic />} />
+                  <Route path="/admin/class-manage" element={<ClassManage />} />
+                  <Route path="/admin/testday-manage" element={<TestdayManage />} />
+                  <Route path="/admin/student-manage" element={<StudentManage />} />
+                  <Route path="/admin/schedule-manage" element={<ScheduleManage />} />
+                  <Route path="/admin/semester-manage" element={<SemesterManage />} />
+                </>
+              )}
             </Routes>
           </div>
-
           <Footer />
         </div>
       </div>
-    </BrowserRouter>
   );
 }
 
